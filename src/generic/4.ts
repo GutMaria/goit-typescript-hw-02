4f634010845351360f083e471e897e110d1709a3
GutMaria <gootmaria@gmail.com>
2024-02-29 14:00:46 +0200

    generic types

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,14 +1,18 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface IPage {
+  title: string
+}
 
-class Component {
+
+class Component <T> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component <IPage>{
   pageInfo () {
     console.log(this.props.title);
   }
